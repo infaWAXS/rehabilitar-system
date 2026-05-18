7ca0c300efdf4514e309ef9989c6ae78684397ab
AgustinAvila <simonynoa34@gmail.com>
2026-05-18 20:13:36 -0300

    Revert "Agregar flujo de certificados médicos"
    This reverts commit a86bb79314e10e19a7f88520641b4371fa59f0fe.
    
    voy a revertir el merge porque no proble las funcionalidades todavia

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -3,7 +3,6 @@ from sqlalchemy.sql import func
 from database.connection import Base
 
 
-
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
@@ -13,9 +12,8 @@ class User(Base):
     password = Column(String, nullable=False)
     dni = Column(String, nullable=True)
     role = Column(String, nullable=False, default="client")
-    account_status = Column(String, nullable=False, default="Active")
+    account_status = Column(String, nullable=False, default="active")
     dni_verified = Column(Boolean, default=False)
-    medical_certificate_path = Column(String, nullable=True)
-    medical_certificate_status = Column(String, default="none")
+    physical_clearance_status = Column(String, default="none")
     failed_login_attempts = Column(Integer, default=0)
     created_at = Column(DateTime, server_default=func.now())
--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,6 +1,5 @@
 from urllib import request
 
-from backend.app.schemas.userSchema import UserLogin
 from fastapi import HTTPException
 
 from sqlalchemy.orm import Session
@@ -50,44 +49,24 @@ def register_user(user_data, db: Session):
 #Si el email existe, verifica si la contraseña es correcta. 
 #Si la contraseña es incorrecta, lanza una excepción HTTP 401. 
 #Si la contraseña es correcta, genera un token de acceso JWT y lo devuelve en la respuesta.       
-def login_user(request: UserLogin, db: Session):
-
+def login_user(user_data, db: Session):
 
     existing_user = db.query(User).filter(
-        User.email == request.email
+        User.email == user_data.email
     ).first()
 
     if not existing_user:
+
         raise user_not_found_exception()
-    
-
-    if existing_user.account_status != "active":
-
-        raise HTTPException(
-            status_code=403,
-            detail="Cuenta deshabilitada"
-        )
-
 
     password_correct = verify_password(
-        request.password,
+        user_data.password,
         existing_user.password
     )
-    
+
     if not password_correct:
-        existing_user.failed_login_attempts += 1
 
-        if existing_user.failed_login_attempts >= 3:
-            existing_user.account_status = "disabled"
-
-        db.commit()
-        
         raise unauthorized_exception()
-    
-    
-    existing_user.failed_login_attempts = 0
-
-    db.commit()
     
     access_token = create_access_token(
         data={
@@ -100,8 +79,6 @@ def login_user(request: UserLogin, db: Session):
         "token_type": "bearer"
     }
     
-
-
 
 #Cambia la contraseña del usuario actual. 
 def change_password(current_user: User, new_password: str, confirm_password: str, db: Session):
@@ -121,88 +98,12 @@ def change_password(current_user: User, new_password: str, confirm_password: str
         "message": "Contraseña cambiada correctamente"
     }
     
-    
-    
-    
-    
-#Actualiza el nombre y apellido del usuario actual.
 def update_user_info(current_user: User, name: str, lastname: str, db: Session):
     if request.name is not None:
         current_user.name = request.name
 
     if request.lastname is not None:
         current_user.lastname = request.lastname
-    
-    db.commit()
-
-    db.refresh(current_user)
-
-    return current_user
-
-
-#Devuelve una lista con todos los usuarios registrados en la base de datos. 
-#Permite filtrar por rol y estado de cuenta. Solo los admins pueden acceder a esta ruta.
-def get_all_users(db: Session, role: str = None, status: str = None):
-
-    query = db.query(User)
-
-    if role:
-        query = query.filter(User.role == role)
-
-    if status: 
-        query = query.filter(User.account_status == status)
-
-    return query.all()
-
-
-#Devuelve la información de un usuario específico por su ID.
-def get_user_by_id(user_id: int, db: Session):
-    user = db.query(User).filter(
-        User.id == user_id
-    ).first()
-
-    if not user:
-
-        raise user_not_found_exception()
-
-    return user
-
-
-#Cambia el estado de la cuenta de un usuario (activo o deshabilitado). Solo los admins pueden realizar esta acción.
-def change_user_status(user_id: int, status: str, db: Session):
-    
-    user = db.query(User).filter(
-        User.id == user_id
-    ).first()
-
-    if not user:
-        raise user_not_found_exception()
-    user.account_status = status
-
-    db.commit()
-
-    db.refresh(user)
-
-    return user
-    
         
-
-def change_medical_clearance_status(user_id: int, db: Session):
-
-    user = db.query(User).filter(
-        User.id == user_id
-    ).first()
-
-    if not user:
-        raise user_not_found_exception()
-
-    user.physical_clearance_status = "approved"
-
-    db.commit()
-
-    db.refresh(user)
-
-    return user
-
-
+        
 
