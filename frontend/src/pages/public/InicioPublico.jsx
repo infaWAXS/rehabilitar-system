2b0787bc7594fa322f48a727a3c0dbeb69047391
FrancisJPV <palacios.fjpv@gmail.com>
2026-05-26 03:46:36 -0300

    fix ui

--- frontend/src/pages/public/InicioPublico.jsx
+++ frontend/src/pages/public/InicioPublico.jsx
@@ -15,7 +15,6 @@ const MENUS_ROL = {
   ],
   professor: [
     { label: 'Actividades', ruta: '/profesor/actividades' },
-    { label: 'Asistencias', ruta: '/profesor/asistencias' },
     { label: 'Mi Perfil',   ruta: '/perfil' },
   ],
   receptionist: [
